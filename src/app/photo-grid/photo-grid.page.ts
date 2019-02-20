785e9e149b49dbd5ad4bb22e87817d30e3cb37f6
James Doc <jamesdoc@gmail.com>
2019-02-20 17:58:57 +0000

    Add question to grid screen

--- src/app/photo-grid/photo-grid.page.ts
+++ src/app/photo-grid/photo-grid.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { DataService, PhotoService } from '../data.service';
+import { DataService, PhotoService, QuestionService } from '../data.service';
+import { Photo, Question } from '../descriptions';
+import n2w from 'number-to-words';
 
 @Component({
   selector: 'app-photo-grid',
@@ -8,11 +10,15 @@ import { DataService, PhotoService } from '../data.service';
   styleUrls: ['./photo-grid.page.scss'],
 })
 export class PhotoGridPage implements OnInit {
-  photos: {};
+  photos: Photo[];
+  questions: Question[];
+  question: Question;
   photo: number;
+  n2wConverter = n2w;
 
   constructor(
     private photoService: PhotoService,
+    private questionService: QuestionService,
     private router: Router,
     public data:DataService
   ) {
@@ -20,11 +26,20 @@ export class PhotoGridPage implements OnInit {
 
   ngOnInit() {
     this.getPhotos();
+    this.getQuestions();
+
     this.data.currentPhoto.subscribe(photo => this.photo = photo);
+    this.data.currentQuestion.subscribe(questionId => {
+      this.question = this.questions[questionId];
+    })
   }
 
   getPhotos(): void {
     this.photos = this.photoService.getPhotos();
+  }
+
+  getQuestions(): void {
+    this.questions = this.questionService.getQuestions();
   }
 
   btnPhoto(id: number) {
