df5203be6ce7577606fa901e5dbda950a246eb2e
James Doc <jamesdoc@gmail.com>
2019-02-21 12:26:53 +0000

    Animate the landing screen

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { PhotoService } from '../data.service';
+import { Photo } from '../descriptions';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
+export class HomePage {
+  photos: Photo[];
+  photo: Photo;
+
+  constructor(
+    private photoService: PhotoService
+  ){}
+
+  ngOnInit() {
+    this.getPhotos();
+  }
+
+  getPhotos(): void {
+    this.photos = this.photoService.getPhotos();
+  }
 }
